18df5d7a04aee9a05f431dbfe4342fdd468169ea
alperdelen-admin <alperdelen.dev@gmail.com>
2025-05-07 14:59:11 +0300

    +created: FirstScript script

---
+++ Assets/FirstScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LessonBasic
+{
+    public class FirstScript : MonoBehaviour
+    {
+        [SerializeField] private int testInt = 2;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
